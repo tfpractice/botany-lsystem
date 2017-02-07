a97fefebd5544d2f7d1fd6debb7f4b15637047da
theory friction practice <jackson.brebnor@gmail.com>
2017-02-07 11:32:57 -0500

    turtle tests

--- __tests__/turtle.test.js
+++ __tests__/turtle.test.js
@@ -1,10 +1,45 @@
 import 'jasmine-expect';
-import { forward, rotate, state, transX, transY, } from 'src/turtle';
+import { forward, getDir,getX,getY,rotate, state, transX, transY, } from 'src/turtle';
 
 describe('turtle', () => {
   describe('state', () => {
     it('returns an object with x,y and dir props', () => {
-      expect(state(1,1, Math.PI / 2)).toBeObject();
+      expect(state(1,2, Math.PI / 2)).toBeObject();
+    });
+  });
+  describe('getX', () => {
+    it('returns the x-value of a turtle state', () => {
+      expect(getX(state(1,2, Math.PI / 2))).toEqual(1);
+    });
+  });
+  describe('getY', () => {
+    it('returns the y-value of a turtle state', () => {
+      expect(getY(state(1,2, Math.PI / 2))).toEqual(2);
+    });
+  });
+  describe('getDir', () => {
+    it('returns the x-value of a turtle state', () => {
+      expect(getDir(state(1,2, Math.PI / 2))).toEqual(Math.PI / 2);
+    });
+  });
+  describe('rotate', () => {
+    it('returns a new state with direction changed', () => {
+      expect(getDir(rotate(Math.PI / 2)(state(1,2, Math.PI / 2)))).toEqual(Math.PI);
+    });
+  });
+  describe('transX', () => {
+    it('translates the x-value of a turtle state', () => {
+      expect(Math.round(transX(3)(state(1,2, Math.PI / 2)))).toEqual(1);
+    });
+  });
+  describe('transY', () => {
+    it('translates the y-value of a turtle state', () => {
+      expect(transY(3)(state(1,2, Math.PI / 2))).toEqual(5);
+    });
+  });
+  describe('forward', () => {
+    it('returns a new state with x and y changed', () => {
+      expect(getDir(forward(3)(state(1,2, Math.PI / 2)))).toEqual(Math.PI / 2);
     });
   });
 });
--- src/turtle.js
+++ src/turtle.js
@@ -1,9 +1,11 @@
-const { cos, sin, } = Math;
+const { cos, sin, PI, } = Math;
 
 export const state = (x = 0, y = 0, dir = 0) => ({ x, y, dir, });
-export const transX = m => ({ x, y, dir, }) => (x + (m * cos(dir)));
-export const transY = m => ({ x, y, dir, }) => (y + (m * sin(dir)));
+export const getX = ({ x = 0, }) => x;
+export const getY = ({ y = 0, }) => y;
+export const getDir = ({ dir = 0, }) => dir;
+export const transX = mag => s => (getX(s) + (mag * cos(getDir(s))));
+export const transY = mag => s => (getY(s) + (mag * sin(getDir(s))));
 
-export const rotate = delta => ({ x, y, dir, }) => state(x, y, delta + dir);
-export const forward = m => ({ x, y, dir, }) =>
-state(transX(m)(x), transY(m)(y), dir);
+export const rotate = delta => s => state(getX(s),getY(s), getDir(s) + delta);
+export const forward = mag => s => state(transX(mag)(s), transY(mag)(s), getDir(s));
