bedd509cc1382bbf0329c8529e5bb0849b84a7c3
theory friction practice <jackson.brebnor@gmail.com>
2017-02-07 12:04:41 -0500

    system tests

--- __tests__/system.test.js
+++ __tests__/system.test.js
@@ -3,18 +3,18 @@ import { command,successor,system, } from 'src/system';
 
 describe('system', () => {
   describe('system', () => {
-    it('returns an map with', () => {
+    it('returns an map ', () => {
       expect(system() instanceof Map).toBeTrue();
     });
   });
-  describe('getX', () => {
-    it('returns the x-value of a turtle state', () => {
-      // expect(getX(state(1,2, Math.PI / 2))).toEqual(1);
+  describe('successor', () => {
+    it('retrieves the successor string of a system key', () => {
+      expect(successor(system())('a')).toEqual('a');
     });
   });
-  describe('getY', () => {
+  describe('command', () => {
     it('returns the y-value of a turtle state', () => {
-      // expect(getY(state(1,2, Math.PI / 2))).toEqual(2);
+      expect(command(system())('a')).toBeFunction();
     });
   });
   describe('getDir', () => {
