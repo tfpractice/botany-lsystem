967ac9f6a111a153e4c8415d3ddc5dc0ba5d3395
theory friction practice <jackson.brebnor@gmail.com>
2017-02-07 09:37:16 -0500

    cleannup

--- src/dol/dol.js
+++ src/dol/dol.js
@@ -1,7 +1,8 @@
 import { addMap, asMap,asSet,get, } from 'fenugreek-collections';
 
-console.log('asMap', asMap);
 export const DOL = (...terms) => asMap(asSet(terms));
-export const setSuccessor = system => pred => succ =>
-addMap(system)(pred)(succ);
+
+export const setSuccessor = sys => pred => succ =>
+  addMap(sys)(pred)(succ);
+  
 export const successor = sys => pred => get(sys)(pred);
