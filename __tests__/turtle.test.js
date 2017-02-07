81369f8a5c7ff2e058cfb549ac5ef75ee64b3487
theory friction practice <jackson.brebnor@gmail.com>
2017-02-07 14:41:09 -0500

    pre integration

--- __tests__/turtle.test.js
+++ __tests__/turtle.test.js
@@ -1,45 +1,71 @@
 import 'jasmine-expect';
-import { forward, getDir,getX,getY,rotate, state, transX, transY, } from 'src/turtle';
+import { forward, getDelta,getDir,getMag,getX, getY, interpretBin, rotate,
+  state, transX, transY,vector, } from 'src/turtle';
+const myState = state(1,2, Math.PI / 2);
+const myVector = vector(3, Math.PI / 2);
 
 describe('turtle', () => {
   describe('state', () => {
     it('returns an object with x,y and dir props', () => {
-      expect(state(1,2, Math.PI / 2)).toBeObject();
+      expect(myState).toBeObject();
     });
   });
   describe('getX', () => {
     it('returns the x-value of a turtle state', () => {
-      expect(getX(state(1,2, Math.PI / 2))).toEqual(1);
+      expect(getX(myState)).toEqual(1);
     });
   });
   describe('getY', () => {
     it('returns the y-value of a turtle state', () => {
-      expect(getY(state(1,2, Math.PI / 2))).toEqual(2);
+      expect(getY(myState)).toEqual(2);
     });
   });
   describe('getDir', () => {
     it('returns the x-value of a turtle state', () => {
-      expect(getDir(state(1,2, Math.PI / 2))).toEqual(Math.PI / 2);
+      expect(getDir(myState)).toEqual(Math.PI / 2);
+    });
+  });
+  describe('vector', () => {
+    it('returns an object with magnitude and delta', () => {
+      expect(vector(10, Math.PI / 3)).toBeObject();
+    });
+  });
+  describe('getMag', () => {
+    it('returns the vectors magnitude', () => {
+      expect(getMag(vector(10, Math.PI / 3))).toEqual(10);
+    });
+  });
+  describe('getDelta', () => {
+    it('returns the vectors magnitude', () => {
+      expect(getDelta(vector(10, Math.PI / 3))).toEqual(Math.PI / 3);
     });
   });
   describe('rotate', () => {
     it('returns a new state with direction changed', () => {
-      expect(getDir(rotate(Math.PI / 2)(state(1,2, Math.PI / 2)))).toEqual(Math.PI);
+      expect(getDir(rotate(myVector)(myState))).toEqual(Math.PI);
     });
   });
   describe('transX', () => {
     it('translates the x-value of a turtle state', () => {
-      expect(Math.round(transX(3)(state(1,2, Math.PI / 2)))).toEqual(1);
+      expect(Math.round(transX(myVector)(myState))).toEqual(1);
     });
   });
   describe('transY', () => {
     it('translates the y-value of a turtle state', () => {
-      expect(transY(3)(state(1,2, Math.PI / 2))).toEqual(5);
+      expect(transY(myVector)(myState)).toEqual(5);
     });
   });
   describe('forward', () => {
     it('returns a new state with x and y changed', () => {
-      expect(getDir(forward(3)(state(1,2, Math.PI / 2)))).toEqual(Math.PI / 2);
+      expect(getDir(forward(myVector)(myState))).toEqual(Math.PI / 2);
+    });
+  });
+  
+  describe('interpretBin', () => {
+    it('returns a new state with x and y changed', () => {
+      console.log('interpretBin(myState, forward(myVector)', interpretBin(myState, forward(myVector)));
+
+      // expect((interpretBin(myState, forward(myVector)).toEqual(Math.PI / 2);
     });
   });
 });
--- src/turtle.js
+++ src/turtle.js
@@ -5,14 +5,18 @@ export const state = (x = 0, y = 0, dir = 0) => ({ x, y, dir, });
 export const getX = ({ x = 0, }) => x;
 export const getY = ({ y = 0, }) => y;
 export const getDir = ({ dir = 0, }) => dir;
-export const transX = mag => s => getX(s) + (mag * cos(getDir(s)));
-export const transY = mag => s => getY(s) + (mag * sin(getDir(s)));
-export const rotate = delta => s => state(getX(s), getY(s), getDir(s) + delta);
-export const forward = mag => s => state(transX(mag)(s), transY(mag)(s), getDir(s));
 export const vector = (mag = 0, delta = 0) => ({ mag, delta, });
 export const getMag = ({ mag = 0, }) => mag;
 export const getDelta = ({ delta = 0, }) => delta;
-export const setForward = mag => sys => term => setComm(sys)(term)(forward(mag));
-export const setDelta = delta => sys => term => setComm(sys)(term)(rotate(mag));
+
+export const transX = v => s => getX(s) + (getMag(v) * cos(getDir(s)));
+export const transY = v => s => getY(s) + (getMag(v) * sin(getDir(s)));
+export const rotate = v => s => state(getX(s), getY(s), getDir(s) + getDelta(v));
+export const forward = v => s => state(transX(v)(s), transY(v)(s), getDir(s));
+
+export const setForward = sys => term => v => setComm(sys)(term)(forward(mag));
+export const setDelta = sys => term => v => setComm(sys)(term)(rotate(mag));
+export const interpret = sys => term => v => state => command(sys)(term)(v)(state);
+export const interpretBin = (state, command) => command(state);
 
 // export const setVector = ({ mag, delta, }) => sys => term;
