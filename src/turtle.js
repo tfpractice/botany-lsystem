64a82b6293908160d16295e0f8825ad501b0effe
theory friction practice <jackson.brebnor@gmail.com>
2017-02-07 11:36:15 -0500

    linting

--- src/turtle.js
+++ src/turtle.js
@@ -1,11 +1,10 @@
-const { cos, sin, PI, } = Math;
+const { cos, sin, } = Math;
 
 export const state = (x = 0, y = 0, dir = 0) => ({ x, y, dir, });
 export const getX = ({ x = 0, }) => x;
 export const getY = ({ y = 0, }) => y;
 export const getDir = ({ dir = 0, }) => dir;
-export const transX = mag => s => (getX(s) + (mag * cos(getDir(s))));
-export const transY = mag => s => (getY(s) + (mag * sin(getDir(s))));
-
-export const rotate = delta => s => state(getX(s),getY(s), getDir(s) + delta);
+export const transX = mag => s => getX(s) + (mag * cos(getDir(s)));
+export const transY = mag => s => getY(s) + (mag * sin(getDir(s)));
+export const rotate = delta => s => state(getX(s), getY(s), getDir(s) + delta);
 export const forward = mag => s => state(transX(mag)(s), transY(mag)(s), getDir(s));
