9db95482a14cd9f080711af66898a6ae6543fa8b
theory friction practice <jackson.brebnor@gmail.com>
2017-02-09 18:18:42 -0500

    concatCall

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,9 @@ export const kestrel = x => () => x;
 export const isFunc = fn => fn instanceof Function;
 export const callOn = x => fn => isFunc(fn) ? fn(x) : x;
 export const callBin = (x, fn = identity) => callOn(x)(fn);
+export const concatCall = (arr = [], fn) => arr.concat(fn(lastV(arr)));
+export const concatCallBin = (vals = [], fn) => vals.concat(fn(lastV(vals)));
 
-// export const result
 export const pipeline = (...funcs) => x => funcs.reduce(callBin, x);
 
-// export const spreadPipe =(...funcs)=> x=> funcs.red
+export const spreadPipe = (...funcs) => x => funcs.reduce(concatCall, [ x, ]);
